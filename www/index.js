258001f28c50aec15ebc3e2f603847512ce82b4c
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-02-27 19:53:01 +0000

    move fps and consts to own files

--- www/index.js
+++ www/index.js
@@ -1,12 +1,15 @@
 import { Universe, Cell } from 'wasm-game-of-life';
 import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
-
-const WIDTH = 500;
-const HEIGHT = 500;
-const CELL_SIZE = 5; // px
-const GRID_COLOR = '#CCCCCC';
-const DEAD_COLOR = '#FFFFFF';
-const ALIVE_COLOR = '#000000';
+import {
+    WIDTH,
+    HEIGHT,
+    CELL_SIZE,
+    GRID_COLOR,
+    DEAD_COLOR,
+    ALIVE_COLOR,
+} from './src/constants';
+import { FPS } from './src/fps';
+import { GameController } from './src/gameController';
 
 // Construct the universe, and get its width and height.
 const universe = Universe.new(WIDTH, HEIGHT);
@@ -20,49 +23,6 @@ canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext('2d');
-
-class FPS {
-    constructor() {
-        this.fps = document.getElementById('fps');
-        this.frames = [];
-        this.lastFrameTimeStamp = performance.now();
-    }
-
-    render() {
-        // Convert the delta time since the last frame render into a measure
-        // of frames per second.
-        const now = performance.now();
-        const delta = now - this.lastFrameTimeStamp;
-        this.lastFrameTimeStamp = now;
-        const fps = (1 / delta) * 1000;
-
-        // Save only the latest 100 timings.
-        this.frames.push(fps);
-        if (this.frames.length > 100) {
-            this.frames.shift();
-        }
-
-        // Find the max, min, and mean of our 100 latest timings.
-        let min = Infinity;
-        let max = -Infinity;
-        let sum = 0;
-        for (let i = 0; i < this.frames.length; i++) {
-            sum += this.frames[i];
-            min = Math.min(this.frames[i], min);
-            max = Math.max(this.frames[i], max);
-        }
-        let mean = sum / this.frames.length;
-
-        // Render the statistics.
-        this.fps.textContent = `
-  Frames per Second:
-           latest = ${Math.round(fps)}
-  avg of last 100 = ${Math.round(mean)}
-  min of last 100 = ${Math.round(min)}
-  max of last 100 = ${Math.round(max)}
-  `.trim();
-    }
-}
 
 const fps = new FPS();
 
---
+++ www/src/constants.js
@@ -0,0 +1,7 @@
+export const WIDTH = 500;
+export const HEIGHT = 500;
+
+export const CELL_SIZE = 5; // px
+export const GRID_COLOR = '#CCCCCC';
+export const DEAD_COLOR = '#FFFFFF';
+export const ALIVE_COLOR = '#000000';
